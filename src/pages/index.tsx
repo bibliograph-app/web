09f5b3b5b77cdb05be4c7b846bc1b16a2632a3f9
SnO2WMaN <me@sno2wman.net>
2022-10-01 17:03:44 +0900

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+const Page: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        Init
+      </main>
+    </div>
+  );
+};
+
+export default Page;
