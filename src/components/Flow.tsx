6ef5f7db215d598a35a2b1818a6ef68c04abb973
SnO2WMaN <me@sno2wman.net>
2022-10-04 18:28:34 +0900

    データ渡し

--- src/components/Flow.tsx
+++ src/components/Flow.tsx
@@ -1,14 +1,10 @@
 import clsx from "clsx";
-import React, { useCallback, useMemo } from "react";
+import React, { useMemo } from "react";
 import ReactFlow, {
-  addEdge,
   Background,
-  Connection,
   Controls,
-  Edge,
   Handle,
   MarkerType,
-  MiniMap,
   NodeProps,
   NodeTypes,
   Position,
@@ -19,34 +15,41 @@ import ReactFlow, {
 export const MaterialNode: React.FC<
   NodeProps<{
     title: string;
-    cover: string | null;
+    thumbnail: string | null;
   }>
 > = ({
   data: {
     title,
-    cover,
+    thumbnail: thumbnail,
   },
 }) => {
   return (
     <>
-      <Handle type="target" position={Position.Top} />
       <Handle type="source" position={Position.Bottom} />
+      <Handle type="target" position={Position.Top} />
+      <Handle type="target" position={Position.Left} />
       <div
         className={clsx(
           ["w-24"],
+          ["h-28"],
+          ["p-2"],
           "border",
           ["bg-white"],
-          ["p-2"],
+          ["rounded-md"],
         )}
       >
-        {cover && <img className={clsx([])} src={cover}></img>}
-        {!cover && (
-          <div
+        {thumbnail && (
+          <img
             className={clsx(
-              ["text-xs"],
-              ["leading-tight"],
+              ["mx-auto"],
+              ["h-full"],
             )}
+            src={thumbnail}
           >
+          </img>
+        )}
+        {!thumbnail && (
+          <div className={clsx(["text-xs"], ["leading-tight"])}>
             {title}
           </div>
         )}
@@ -55,229 +58,71 @@ export const MaterialNode: React.FC<
   );
 };
 
-export const Flow: React.FC<{}> = ({}) => {
-  const nodeTypes = useMemo<NodeTypes>(() => ({
-    material: MaterialNode,
-  }), []);
+export const calcPosition = (nest: number[]) => {
+  if (nest.length === 1) {
+    return { y: 0, x: 0 };
+  } else if (nest.length === 2) {
+    return { y: 200, x: nest[1] * 128 };
+  } else {
+    return {};
+  }
+};
 
+export const Flow: React.FC<{
+  data: {
+    nodes: {
+      id: string;
+      title: string;
+      thumbnail: string | null;
+      nest: number[];
+    }[];
+    edges: {
+      id: string;
+      from: string;
+      to: string;
+      type:
+        | { tag: "REFER" }
+        | { tag: "UPDATE" }
+        | { tag: "TRANSLATE"; lang: string };
+    }[];
+  };
+}> = ({ data }) => {
+  const nodeTypes = useMemo<NodeTypes>(() => ({ material: MaterialNode }), []);
   const [nodes, setNodes, onNodesChange] = useNodesState<{
     title: string;
-    cover: string | null;
-  }>([
-    {
-      id: "4e96602b-ebe2-4204-87f1-ddd6144b82e0",
-      position: { x: 0, y: -60 },
-      type: "material",
-      data: {
-        title: "プログラミング言語の基礎概念",
-        cover: "https://cover.openbd.jp/9784781912851.jpg",
-      },
-    },
-    {
-      id: "42b21823-3e33-4c71-9cd1-36f0bc7ffda8",
-      type: "material",
-      position: { x: 0, y: 160 },
-      data: {
-        title: "プログラム意味論",
-        cover: "https://cover.openbd.jp/9784320026575.jpg",
-      },
-    },
-    {
-      id: "189a4fb2-a80a-41b4-9bff-b5e4e7ac1d6f",
-      type: "material",
-      position: { x: 100, y: 160 },
-      data: {
-        title: "プログラム検証論",
-        cover: "https://cover.openbd.jp/9784320026582.jpg",
-      },
-    },
-    {
-      id: "53fedbd2-c358-40ae-8da6-b3a0103313c2",
-      type: "material",
-      position: { x: 200, y: 160 },
-      data: {
-        title: "プログラミング言語の基礎理論",
-        cover: "https://cover.openbd.jp/9784320026599.jpg",
-      },
-    },
-    {
-      id: "a1c04346-5fb6-48bd-b5ac-1d218d4f97ac",
-      type: "material",
-      position: { x: 200, y: 320 },
-      data: {
-        title: "新装版 プログラミング言語の基礎理論",
-        cover: "https://cover.openbd.jp/9784320124509.jpg",
-      },
-    },
-    {
-      id: "fae83103-432a-428f-b809-6331b41cb482",
-      type: "material",
-      position: { x: 300, y: 160 },
-      data: {
-        title: "論理と計算のしくみ",
-        cover: "https://cover.openbd.jp/9784000061919.jpg",
-      },
-    },
-    {
-      id: "cb6e5656-811c-46a8-ac15-54239df5ff3c",
-      type: "material",
-      position: { x: 400, y: 160 },
-      data: {
-        title: "計算論 計算可能性とラムダ計算",
-        cover: null,
-      },
-    },
-    {
-      id: "981ecdfd-5b16-43bd-b0af-0b006e0ff981",
-      type: "material",
-      position: { x: 500, y: 160 },
-      data: {
-        title: "論理とプログラム意味論",
-        cover: null,
-        //  cover: "https://cover.openbd.jp/9784000061902.jpg",
-      },
-    },
-    {
-      id: "cd249629-5cd6-48ea-9581-7b216484a9ed",
-      type: "material",
-      position: { x: 600, y: 160 },
-      data: {
-        title: "アルゴリズムとプログラミング言語",
-        cover: null,
-        // cover: "https://cover.openbd.jp/9784000050067.jpg",
-      },
-    },
-    {
-      id: "689b4c40-cdd9-44c0-b26a-361ee0d07282",
-      type: "material",
-      position: { x: 700, y: 160 },
-      data: {
-        title: "The Formal Semantics of Programming Languages",
-        // temp
-        cover: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/2627/9780262731034.jpg",
-      },
-    },
-    {
-      id: "ef993db3-6df3-48f9-ab23-dc64e2ae8a87",
-      type: "material",
-      position: { x: 800, y: 160 },
-      data: {
-        title: "Types and Programming Languages",
-        cover: "https://covers.openlibrary.org/b/isbn/9780262162098-L.jpg",
-      },
-    },
-    {
-      id: "d1d43a80-a3ca-4338-bbb6-29aeb4f915bb",
-      type: "material",
-      position: { x: 800, y: 320 },
-      data: {
-        title: "型システム入門 プログラミング言語と型の理論",
-        cover: "https://cover.openbd.jp/9784274069116.jpg",
-      },
-    },
-  ]);
+    thumbnail: string | null;
+  }>(
+    data.nodes.map(({ id, title, thumbnail, nest }) => {
+      const position = nest.length === 1
+        ? { x: 0, y: 0 }
+        : (nest.length === 2
+          ? {
+            x: nest[1] * 128,
+            y: 192,
+          }
+          : {
+            x: nest[1] * 128 + 16,
+            y: 384,
+          });
+      return ({
+        id: id,
+        type: "material",
+        position: position,
+        data: { title, thumbnail },
+      });
+    }),
+  );
+
   const [edges, setEdges, onEdgesChange] = useEdgesState(
-    [
-      {
-        id: "e1_1",
-        source: "4e96602b-ebe2-4204-87f1-ddd6144b82e0",
-        target: "42b21823-3e33-4c71-9cd1-36f0bc7ffda8",
-        label: "参照",
-        markerEnd: {
-          type: MarkerType.ArrowClosed,
-        },
+    data.edges.map(({ id, from, to, type }) => ({
+      id: id,
+      source: from,
+      target: to,
+      label: type.tag,
+      markerEnd: {
+        type: MarkerType.ArrowClosed,
       },
-      {
-        id: "e1_2",
-        source: "4e96602b-ebe2-4204-87f1-ddd6144b82e0",
-        target: "189a4fb2-a80a-41b4-9bff-b5e4e7ac1d6f",
-        label: "参照",
-        markerEnd: {
-          type: MarkerType.ArrowClosed,
-        },
-      },
-      {
-        id: "e1_3",
-        source: "4e96602b-ebe2-4204-87f1-ddd6144b82e0",
-        target: "53fedbd2-c358-40ae-8da6-b3a0103313c2",
-        label: "参照",
-        markerEnd: {
-          type: MarkerType.ArrowClosed,
-        },
-      },
-      {
-        id: "e1_4",
-        source: "4e96602b-ebe2-4204-87f1-ddd6144b82e0",
-        target: "cb6e5656-811c-46a8-ac15-54239df5ff3c",
-        label: "参照",
-        markerEnd: {
-          type: MarkerType.ArrowClosed,
-        },
-      },
-      {
-        id: "e1_5",
-        source: "4e96602b-ebe2-4204-87f1-ddd6144b82e0",
-        target: "fae83103-432a-428f-b809-6331b41cb482",
-        label: "参照",
-        markerEnd: {
-          type: MarkerType.ArrowClosed,
-        },
-      },
-      {
-        id: "e1_6",
-        source: "4e96602b-ebe2-4204-87f1-ddd6144b82e0",
-        target: "981ecdfd-5b16-43bd-b0af-0b006e0ff981",
-        label: "参照",
-        markerEnd: {
-          type: MarkerType.ArrowClosed,
-        },
-      },
-      {
-        id: "e1_7",
-        source: "4e96602b-ebe2-4204-87f1-ddd6144b82e0",
-        target: "cd249629-5cd6-48ea-9581-7b216484a9ed",
-        label: "参照",
-        markerEnd: {
-          type: MarkerType.ArrowClosed,
-        },
-      },
-      {
-        id: "e1_8",
-        source: "4e96602b-ebe2-4204-87f1-ddd6144b82e0",
-        target: "689b4c40-cdd9-44c0-b26a-361ee0d07282",
-        label: "参照",
-        markerEnd: {
-          type: MarkerType.ArrowClosed,
-        },
-      },
-      {
-        id: "e1_9",
-        source: "4e96602b-ebe2-4204-87f1-ddd6144b82e0",
-        target: "ef993db3-6df3-48f9-ab23-dc64e2ae8a87",
-        label: "参照",
-        markerEnd: {
-          type: MarkerType.ArrowClosed,
-        },
-      },
-      {
-        id: "e2_1",
-        source: "53fedbd2-c358-40ae-8da6-b3a0103313c2",
-        target: "a1c04346-5fb6-48bd-b5ac-1d218d4f97ac",
-        label: "改訂",
-        markerEnd: {
-          type: MarkerType.ArrowClosed,
-        },
-      },
-      {
-        id: "e3_1",
-        source: "ef993db3-6df3-48f9-ab23-dc64e2ae8a87",
-        target: "d1d43a80-a3ca-4338-bbb6-29aeb4f915bb",
-        label: "日本語への翻訳",
-        markerEnd: {
-          type: MarkerType.ArrowClosed,
-        },
-      },
-    ],
+    })),
   );
 
   return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,50 @@
 import clsx from "clsx";
 import { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
 import Head from "next/head";
+import { useMemo } from "react";
 import { Flow } from "~/components/Flow";
 
 export const getStaticProps: GetStaticProps<
   {
     id: string;
     title: string;
-    forwardRelations: {};
+    type: { tag: "BOOK"; cover: string | null };
+    relations: {
+      edges: {
+        cursor: string;
+        node: {
+          id: string;
+          type:
+            | { tag: "REFER" }
+            | { tag: "UPDATE" }
+            | { tag: "TRANSLATE"; lang: string };
+          from: { id: string };
+          to: {
+            id: string;
+            title: string;
+            type: { tag: "BOOK"; cover: string | null };
+            relations: {
+              edges: {
+                cursor: string;
+                node: {
+                  id: string;
+                  type:
+                    | { tag: "REFER" }
+                    | { tag: "UPDATE" }
+                    | { tag: "TRANSLATE"; lang: string };
+                  from: { id: string };
+                  to: {
+                    id: string;
+                    title: string;
+                    type: { tag: "BOOK"; cover: string | null };
+                  };
+                };
+              }[];
+            };
+          };
+        };
+      }[];
+    };
   }
 > = ({ params }) => {
   return {
@@ -18,26 +55,10 @@ export const getStaticProps: GetStaticProps<
         tag: "BOOK",
         cover: "https://cover.openbd.jp/9784781912851.jpg",
       },
-      forwardRelations: {
+      relations: {
         edges: [
           {
             cursor: "1",
-            node: {
-              id: "9a899e4f-ab32-4aba-8464-1e8a26fdbe3e",
-              type: { tag: "REFER" },
-              from: { id: "4e96602b-ebe2-4204-87f1-ddd6144b82e0" },
-              to: {
-                id: "fae83103-432a-428f-b809-6331b41cb482",
-                title: "論理と計算のしくみ",
-                type: {
-                  tag: "BOOK",
-                  cover: "https://cover.openbd.jp/9784000061919.jpg",
-                },
-              },
-            },
-          },
-          {
-            cursor: "2",
             node: {
               id: "2d85d16c-bc23-4730-abb3-f597c3cb10c3",
               type: { tag: "REFER" },
@@ -49,29 +70,183 @@ export const getStaticProps: GetStaticProps<
                   tag: "BOOK",
                   cover: "https://cover.openbd.jp/9784320026575.jpg",
                 },
+                relations: {
+                  edges: [
+                    {
+                      cursor: "1.1",
+                      node: {
+                        id: "5b32d003-9f86-4d27-967e-a10438a29950",
+                        type: { tag: "REFER" },
+                        from: { id: "42b21823-3e33-4c71-9cd1-36f0bc7ffda8" },
+                        to: {
+                          id: "a8bd789b-67b7-4bf7-9aca-7d6554c265db",
+                          title: "計算論 計算可能性とラムダ計算",
+                          type: {
+                            tag: "BOOK",
+                            cover: null,
+                          },
+                        },
+                      },
+                    },
+                  ],
+                },
+              },
+            },
+          },
+          {
+            cursor: "2",
+            node: {
+              id: "6ad32316-fef5-4904-8a21-09ddd673319a",
+              type: { tag: "REFER" },
+              from: { id: "4e96602b-ebe2-4204-87f1-ddd6144b82e0" },
+              to: {
+                id: "189a4fb2-a80a-41b4-9bff-b5e4e7ac1d6f",
+                title: "プログラム検証論",
+                type: {
+                  tag: "BOOK",
+                  cover: "https://cover.openbd.jp/9784320026582.jpg",
+                },
+                relations: {
+                  edges: [],
+                },
               },
             },
           },
           {
             cursor: "3",
             node: {
-              id: "66787cf5-a22a-4a2e-9ec8-0310b3616bca",
+              id: "d4b806f2-e5dc-4e9f-9ddf-b25584405dbe",
               type: { tag: "REFER" },
               from: { id: "4e96602b-ebe2-4204-87f1-ddd6144b82e0" },
               to: {
-                id: "cb6e5656-811c-46a8-ac15-54239df5ff3c",
-                title: "計算論 計算可能性とラムダ計算",
+                id: "53fedbd2-c358-40ae-8da6-b3a0103313c2",
+                title: "プログラミング言語の基礎理論",
                 type: {
                   tag: "BOOK",
-                  cover: null,
-                  // cover: "https://covers.openlibrary.org/b/isbn/9784764901841-L.jpg",
-                  // cover: "https://cover.openbd.jp/9784764901841.jpg",
+                  cover: "https://cover.openbd.jp/9784320026599.jpg",
+                },
+                relations: {
+                  edges: [
+                    {
+                      cursor: "3.1",
+                      node: {
+                        id: "f15a1c83-383c-4b01-b043-ea1877d85dc0",
+                        type: { tag: "UPDATE" },
+                        from: {
+                          id: "53fedbd2-c358-40ae-8da6-b3a0103313c2",
+                        },
+                        to: {
+                          id: "a1c04346-5fb6-48bd-b5ac-1d218d4f97ac",
+                          title: "新装版 プログラミング言語の基礎理論",
+                          type: {
+                            tag: "BOOK",
+                            cover: "https://cover.openbd.jp/9784320124509.jpg",
+                          },
+                        },
+                      },
+                    },
+                  ],
                 },
               },
             },
           },
           {
             cursor: "4",
+            node: {
+              id: "9a899e4f-ab32-4aba-8464-1e8a26fdbe3e",
+              type: { tag: "REFER" },
+              from: { id: "4e96602b-ebe2-4204-87f1-ddd6144b82e0" },
+              to: {
+                id: "fae83103-432a-428f-b809-6331b41cb482",
+                title: "論理と計算のしくみ",
+                type: {
+                  tag: "BOOK",
+                  cover: "https://cover.openbd.jp/9784000061919.jpg",
+                },
+                relations: {
+                  edges: [],
+                },
+              },
+            },
+          },
+          {
+            cursor: "5",
+            node: {
+              id: "cdb562fe-88a4-433d-9ece-2395ca90e540",
+              type: { tag: "REFER" },
+              from: { id: "4e96602b-ebe2-4204-87f1-ddd6144b82e0" },
+              to: {
+                id: "a8bd789b-67b7-4bf7-9aca-7d6554c265db",
+                title: "計算論 計算可能性とラムダ計算",
+                type: {
+                  tag: "BOOK",
+                  cover: null,
+                },
+                relations: {
+                  edges: [],
+                },
+              },
+            },
+          },
+          {
+            cursor: "6",
+            node: {
+              id: "c4fb0c99-21b3-4686-b9ba-9e54ca990fb4",
+              type: { tag: "REFER" },
+              from: { id: "4e96602b-ebe2-4204-87f1-ddd6144b82e0" },
+              to: {
+                id: "981ecdfd-5b16-43bd-b0af-0b006e0ff981",
+                title: "論理とプログラム意味論",
+                type: {
+                  tag: "BOOK",
+                  cover: null,
+                },
+                relations: {
+                  edges: [],
+                },
+              },
+            },
+          },
+          {
+            cursor: "7",
+            node: {
+              id: "97c1c71e-c36b-450b-b66f-82fcb306dd42",
+              type: { tag: "REFER" },
+              from: { id: "4e96602b-ebe2-4204-87f1-ddd6144b82e0" },
+              to: {
+                id: "cd249629-5cd6-48ea-9581-7b216484a9ed",
+                title: "アルゴリズムとプログラミング言語",
+                type: {
+                  tag: "BOOK",
+                  cover: null,
+                },
+                relations: {
+                  edges: [],
+                },
+              },
+            },
+          },
+          {
+            cursor: "8",
+            node: {
+              id: "d93fb267-312d-479d-b5e1-122bee93ce19",
+              type: { tag: "REFER" },
+              from: { id: "4e96602b-ebe2-4204-87f1-ddd6144b82e0" },
+              to: {
+                id: "689b4c40-cdd9-44c0-b26a-361ee0d07282",
+                title: "The Formal Semantics of Programming Languages",
+                type: {
+                  tag: "BOOK",
+                  cover: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/2627/9780262731034.jpg",
+                },
+                relations: {
+                  edges: [],
+                },
+              },
+            },
+          },
+          {
+            cursor: "9",
             node: {
               id: "5750d6b0-8513-4f12-81cb-af793efd2903",
               type: { tag: "REFER" },
@@ -83,6 +258,26 @@ export const getStaticProps: GetStaticProps<
                   tag: "BOOK",
                   cover: "https://covers.openlibrary.org/b/isbn/9780262162098-L.jpg",
                 },
+                relations: {
+                  edges: [
+                    {
+                      cursor: "9.1",
+                      node: {
+                        id: "c1fe5756-d1ad-47c2-b978-1757585b40c8",
+                        type: { tag: "TRANSLATE", lang: "ja" },
+                        from: { id: "ef993db3-6df3-48f9-ab23-dc64e2ae8a87" },
+                        to: {
+                          id: "d1d43a80-a3ca-4338-bbb6-29aeb4f915bb",
+                          title: "型システム入門 プログラミング言語と型の理論",
+                          type: {
+                            tag: "BOOK",
+                            cover: "https://cover.openbd.jp/9784274069116.jpg",
+                          },
+                        },
+                      },
+                    },
+                  ],
+                },
               },
             },
           },
@@ -92,11 +287,82 @@ export const getStaticProps: GetStaticProps<
   };
 };
 
-const Page: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
-  id,
-  title,
-  forwardRelations,
-}) => {
+const Page: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (props) => {
+  const flowdata = useMemo(
+    () => {
+      const combined: {
+        id: string;
+        title: string;
+        thumbnail: string | null;
+        nest: number[];
+        relation: {
+          id: string;
+          from: string;
+          to: string;
+          type:
+            | { tag: "REFER" }
+            | { tag: "UPDATE" }
+            | { tag: "TRANSLATE"; lang: string };
+        };
+      }[] = props.relations.edges
+        .map(({ node: rel1 }, i) => [
+          {
+            id: rel1.to.id,
+            title: rel1.to.title,
+            thumbnail: rel1.to.type.tag === "BOOK" ? rel1.to.type.cover : null,
+            nest: [0, i],
+            relation: { id: rel1.id, type: rel1.type, from: rel1.from.id, to: rel1.to.id },
+          },
+          ...rel1.to.relations.edges.map(({ node: rel2 }, j) => ({
+            id: rel2.to.id,
+            title: rel2.to.title,
+            thumbnail: rel2.to.type.tag === "BOOK" ? rel2.to.type.cover : null,
+            relation: { id: rel2.id, type: rel2.type, from: rel2.from.id, to: rel2.to.id },
+            nest: [0, i, j],
+          })),
+        ])
+        .reduce((p, c) => [...p, ...c], [])
+        .sort(({ nest: nestA }, { nest: nestB }) => nestA.length - nestB.length);
+
+      const nodes: {
+        id: string;
+        title: string;
+        thumbnail: string | null;
+        nest: number[];
+      }[] = [
+        {
+          id: props.id,
+          title: props.title,
+          thumbnail: props.type.cover,
+          nest: [0],
+        },
+        ...combined.map(({ id, title, thumbnail, nest }) => ({
+          id,
+          title,
+          thumbnail,
+          nest,
+        })),
+      ].filter(({ id }, i, arr) => arr.findIndex(({ id: id2 }) => id2 === id) === i);
+
+      const edges: {
+        id: string;
+        from: string;
+        to: string;
+        type:
+          | { tag: "REFER" }
+          | { tag: "UPDATE" }
+          | { tag: "TRANSLATE"; lang: string };
+      }[] = combined.map(({ relation }) => ({
+        id: relation.id,
+        from: relation.from,
+        to: relation.to,
+        type: relation.type,
+      }));
+      return ({ nodes, edges });
+    },
+    [props],
+  );
+
   return (
     <>
       <Head>
@@ -110,7 +376,7 @@ const Page: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
         )}
       >
         <div className={clsx(["w-full"], ["h-full"])}>
-          <Flow />
+          <Flow data={flowdata} />
         </div>
       </main>
     </>
