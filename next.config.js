07696018f7629559bbe3fcad39307aebe6e1a7fc
SnO2WMaN <me@sno2wman.net>
2022-10-19 00:36:40 +0900

    add `cover.openbd.jp`

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ["page.tsx", "page.ts"],
+  images: {
+    domains: ["cover.openbd.jp"],
+  },
 };
 module.exports = nextConfig;
